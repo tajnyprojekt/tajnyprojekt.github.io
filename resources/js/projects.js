9a7727fdc4457151c8b093efe047feb15c59ffad
tajnyProjekt <najtajniejszyprojekt@gmail.com>
2017-07-04 16:19:48 +0200

    Switching between projects

--- resources/js/projects.js
+++ resources/js/projects.js
@@ -3,19 +3,41 @@ var projects = new Vue({
     data: {
         projectsJsonUrl: 'resources/data/projects.json',
         projects: [],
+        currentProjectIndex: 0,
         currentProject: {}
     },
     created: function () {
       this.loadProjects();
     },
     methods: {
+
+        updateCurrentProject: function() {
+            this.currentProject = this.projects[this.currentProjectIndex];
+        },
+
         loadProjects: function () {
             $.getJSON(this.projectsJsonUrl, function(data) {
                 this.projects = data;
                 if(this.projects.length > 0) {
-                    this.currentProject = this.projects[0];
+                    this.updateCurrentProject();
                 }
             }.bind(this));
+        },
+
+        nextProject: function () {
+            this.currentProjectIndex++;
+            if(this.currentProjectIndex >= this.projects.length) {
+                this.currentProjectIndex = 0;
+            }
+            this.updateCurrentProject();
+        },
+
+        previousProject: function () {
+            this.currentProjectIndex--;
+            if(this.currentProjectIndex < 0) {
+                this.currentProjectIndex = this.projects.length - 1;
+            }
+            this.updateCurrentProject();
         }
     }
 });
