8c8259991c5012754609028ef2df2a303f229ac8
tajnyProjekt <najtajniejszyprojekt@gmail.com>
2017-10-20 02:38:58 +0200

    Applied brutalist concepts

--- resources/js/decorations.js
+++ resources/js/decorations.js
@@ -1,3 +1,6 @@
+var mouseXPrev = 0, mouseYPrev = 0;
+var mouseXNext = 0, mouseYNext = 0;
+var mouseXContact = 0, mouseYContact = 0;
 
 $(document).ready(function () {
 
@@ -15,6 +18,52 @@ $(document).ready(function () {
             projects.handleModalClose();
         });
     });
+
+    $('.contact-container-area').mousemove(function(e){
+        var offset = $('.contact-container-area').offset();
+        mouseXContact = e.pageX - offset.left - 70;
+        mouseYContact = Math.min(e.pageY - offset.top - 190, 350);
+        if (mouseXContact < 0) mouseXContact = 0;
+        if (mouseYContact < 0) mouseYContact = 0;
+    });
+
+    $('#project-navigation__previous').mousemove(function(e){
+        var offset = $('#project-navigation__previous').offset();
+        mouseXPrev = e.pageX - offset.left - 70;
+        mouseYPrev = e.pageY - offset.top - 50;
+        if (mouseXPrev < 0) mouseXPrev = 0;
+        if (mouseYPrev < 0) mouseYPrev = 0;
+    });
+
+    $('#project-navigation__next').mousemove(function(e){
+        var offset = $('#project-navigation__next').offset();
+        mouseXNext = e.pageX - offset.left - 70;
+        mouseYNext = e.pageY - offset.top - 50;
+        if (mouseXNext < 0) mouseXNext = 0;
+        if (mouseYNext < 0) mouseYNext = 0;
+    });
+
+    // cache the selector
+    var followerPrev = $("#previous-project-button");
+    var xpPrev = 0, ypPrev = 0;
+    var followerNext = $("#next-project-button");
+    var xpNext = 0, ypNext = 0;
+    var followerContact = $(".contact-container");
+    var xpContact = 0, ypContact = 0;
+    var loop = setInterval(function(){
+        // change 12 to alter damping higher is slower
+        xpPrev += (mouseXPrev - xpPrev) / 12;
+        ypPrev += (mouseYPrev - ypPrev) / 12;
+        followerPrev.css({left:xpPrev, top:ypPrev});
+
+        xpNext += (mouseXNext - xpNext) / 12;
+        ypNext += (mouseYNext - ypNext) / 12;
+        followerNext.css({left:xpNext, top:ypNext});
+
+        xpContact += (mouseXContact - xpContact) / 12;
+        ypContact += (mouseYContact - ypContact) / 12;
+        followerContact.css({left:xpContact, top:ypContact});
+    }, 10);
 });
 
 function toggleBoxes() {
